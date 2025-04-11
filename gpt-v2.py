18c50554e308b01520b067f65a58d7b374ecdbe5
ChrisPan <szupzj18@outlook.com>
2025-04-11 23:14:50 +0800

    feat: create head for self-attention

--- gpt-v2.py
+++ gpt-v2.py
@@ -105,6 +105,18 @@ def estimate_loss():
 
 # step5: model
 torch.manual_seed(42)
+
+class Head(nn.Module):
+    '''one head of self attention'''
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) # lower triangular matrix
+        
+        
+        
 class BigramLanguageModel(nn.Module):
     
     def __init__(self):
